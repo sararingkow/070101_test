53741a89e3be426ad289bf5ed7b5b31be60c872f
Sara <sape0003@stud.kea.dk>
2024-02-06 19:36:45 +0100

    Changes

---
+++ productlist.js
@@ -0,0 +1,46 @@
+fetch("https://kea-alt-del.dk/t7/api/products?limit=10")
+  .then((response) => response.json())
+  .then(showProductLists);
+
+function showProductLists(products) {
+  products.forEach(showProductList);
+}
+
+function showProductList(product) {
+  console.log(product);
+
+  const template = document.querySelector("#smallProductTemplate").content;
+  const copy = template.cloneNode(true);
+
+  copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  copy.querySelector("h3").textContent = product.productdisplayname;
+  copy.querySelector(".articletype").textContent = product.articletype;
+  copy.querySelector(".productname").textContent = product.brandname;
+  copy.querySelector(".price").textContent = "DKK " + product.price + ",-";
+
+  if (product.soldout) {
+    document.querySelector("article").classList.add("soldOut");
+  }
+
+  document.querySelector("main").appendChild(copy);
+}
+
+/*
+ <main>
+        <h2>Miscellaneous</h2>
+        <template id="smallProductTemplate">
+            <article class="smallProduct">
+                <img src="https://kea-alt-del.dk/t7/images/webp/640/1525.webp" alt="white sweatshirt">
+                <h3>White plain sweatshirt</h3>
+                <p class="subtle"><span class="articletype">Shirts</span> | <span class="productname">Other
+                        Brands</span></p>
+                <p class="price"><span>Prev.</span> DKK 1595,-</p>
+                <div class="discounted">
+                    <p>Now DKK 1560,-</p>
+                    <p>-34%</p>
+                </div>
+                <a class="read-more" href="product.html">Read More</a>
+            </article>
+        </template>
+    </main>
+    */
