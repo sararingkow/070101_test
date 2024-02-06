ee4bec18c8a32252535b22bb8ae094b761633c9b
Sara <sape0003@stud.kea.dk>
2024-02-06 19:06:12 +0100

    Forsøg 2 med js

--- products.js
+++ products.js
@@ -1,20 +1,13 @@
-window.addEventListener("DOMContentLoaded", init);
-
-const URLParams = new URLSearchParams(window.location.search);
-const id = URLParams.get("id");
-
-function init() {
-  fetch("https://kea-alt-del.dk/t7/api/products/" + id)
-    .then((response) => response.json())
-    .then((data) => showProduct(data));
-}
+fetch("https://kea-alt-del.dk/t7/api/products/1525")
+  .then((response) => response.json())
+  .then((data) => showProduct(data));
 
 function showProduct(product) {
-  //console.log(product);
+  console.log(product);
   document.querySelector(".purchaseBox h3").textContent = product.productdisplayname;
   document.querySelector(".purchaseBox .brands").textContent = product.brandname;
   document.querySelector(".purchaseBox .articletype").textContent = product.articletype;
-  document.querySelector(".info .productname").textContent = product.productdisplayname;
-  document.querySelector(".info .productcolor").textContent = product.basecolour;
+  document.querySelector(".productname").textContent = product.productdisplayname;
+  document.querySelector(".productcolor").textContent = product.basecolour;
   document.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
 }
